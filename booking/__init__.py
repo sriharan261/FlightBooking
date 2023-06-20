973aa4dc28a5afec95e91cd2204bcd2a7795cbd1
Sriharan S <38946411+sriharan261@users.noreply.github.com>
2023-06-21 05:24:54 +0530

    Update __init__.py

--- booking/__init__.py
+++ booking/__init__.py
@@ -8,8 +8,8 @@ from flask_login import LoginManager
 
 app=Flask(__name__,template_folder="TEMPLATES")
 app.config['SECRET_KEY'] = 'ed6a264d3d2d25f1061fbea8588d36f5'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://airline_booking_user:eZp329AqLaaO4BjBci6sZCxPtvEesSdR@dpg-ci1a2ju7avjfjalqh5q0-a.oregon-postgres.render.com/airline_booking"
-# app.config['SQLALCHEMY_DATABASE_URI'] ="postgresql://airline_booking_user:eZp329AqLaaO4BjBci6sZCxPtvEesSdR@dpg-ci1a2ju7avjfjalqh5q0-a/airline_booking"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://airline_booking_user:eZp329AqLaaO4BjBci6sZCxPtvEesSdR@dpg-ci1a2ju7avjfjalqh5q0-a.oregon-postgres.render.com/airline_booking"
+app.config['SQLALCHEMY_DATABASE_URI'] ="postgresql://airline_booking_user:eZp329AqLaaO4BjBci6sZCxPtvEesSdR@dpg-ci1a2ju7avjfjalqh5q0-a/airline_booking"
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 bcrypt=Bcrypt(app)
